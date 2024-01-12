4cb7db72eafaa70a57c78b3a9515fb21a8950975
soseeska <astoyan99@mail.ru>
2024-01-12 19:57:45 +0300

    Добавлены темы из библиотеки для UI. Добавлен файл конфигурации. Добавлен кэш.

--- CardDecks.UI/App.xaml.cs
+++ CardDecks.UI/App.xaml.cs
@@ -1,12 +1,23 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Microsoft.Extensions.Configuration;
 using System.Windows;
 
 namespace CardDecks.UI;
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
+
 public partial class App : Application
 {
+    public IServiceProvider Provider { get; }
+
+    public App()
+    {
+        IConfigurationBuilder builder = new ConfigurationBuilder();
+        builder.AddJsonFile("appsettings.json");
+        IConfigurationRoot configuration = builder.Build();
+
+        IServiceCollection services = new ServiceCollection();
+        services.Configure<AppConfiguration>(configuration.GetSection(nameof(AppConfiguration)));
+        services.AddMemoryCache();
+
+        Provider = services.BuildServiceProvider();
+    }
 }
 
