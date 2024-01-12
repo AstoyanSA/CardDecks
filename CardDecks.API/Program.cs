73b051f136ecbdf1de74280bc71f52cd8afba8d1
soseeska <astoyan99@mail.ru>
2024-01-12 17:29:48 +0300

    Исправлена ошибка зацикливания.

--- CardDecks.API/Program.cs
+++ CardDecks.API/Program.cs
@@ -1,10 +1,15 @@
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContextPool<CardDecksDbContext>(options =>
     options.UseInMemoryDatabase("DeckStorage"));
 builder.Services.AddRepositories();
 builder.Services.AddFacades();
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
 
 builder.Services.AddSwaggerGen();
 builder.Services.AddEndpointsApiExplorer();
